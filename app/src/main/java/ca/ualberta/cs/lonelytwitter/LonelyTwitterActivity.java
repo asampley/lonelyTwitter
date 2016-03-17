9abe854a4a940d0044b98390d4a3e75f77b7e292
Alex Sampley <sampley@cs.ualberta.ca>
2016-03-17 15:08:53 -0600

    Adds new tweet to the top

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -64,7 +64,7 @@ public class LonelyTwitterActivity extends Activity {
                 String text = bodyText.getText().toString();
                 NormalTweet latestTweet = new NormalTweet(text);
 
-                tweets.add(latestTweet);
+                tweets.add(0,latestTweet);
 
                 latestTweet.addThumbnail(thumbnail);
                 adapter.notifyDataSetChanged();
